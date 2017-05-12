0949c61e2e55838a751a3f093b00db9ee34ac262
Tomasz Mackiewicz <tomasz.mackiewicz79@gmail.com>
2017-05-12 21:35:20 +0200

    Minor fixes

--- js/showScripts.js
+++ js/showScripts.js
@@ -1,14 +1,14 @@
 function init() {
 
 	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15
+            zoom: 15
 	});
 	var geocoder = new google.maps.Geocoder;
 	var infoWindow = new google.maps.InfoWindow;	
 	var userPosOptions = {
-		enableHighAccuracy: true,
-		timeout: 5000,
-		maximumAge: 0
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
 	};
 
 	function load(position) {
@@ -26,10 +26,10 @@ function init() {
 	        position: markerCoords,
 	        icon: icon
 	    });	    
-		var customLabel = {restaurant: {label: 'R'},bar: {label: 'B'}};
-		var places = document.getElementById('places');
-		var distance = document.getElementById('distInput').value;      
-		var dataUrl = "get.php?dist="+distance+"&lat="+userCoords.latitude+"&lng="+userCoords.longitude;
+            var customLabel = {restaurant: {label: 'R'},bar: {label: 'B'}};
+            var places = document.getElementById('places');
+            var distance = document.getElementById('distInput').value;      
+            var dataUrl = "get.php?dist="+distance+"&lat="+userCoords.latitude+"&lng="+userCoords.longitude;
 	    // Center map on user location
 	    initialLocation = new google.maps.LatLng(userCoords.latitude, userCoords.longitude);
 	    map.setCenter(initialLocation);
@@ -51,68 +51,68 @@ function init() {
 	        }
 	    });
 
-		downloadLocations('get.php', function(data) {
-			places.innerHTML = '';			
-		    var xml = data.responseXML;     
-		    var markers = xml.documentElement.getElementsByTagName('marker');
-		    console.log(markers);
-		    if(markers.length === 0) {
-		      places.innerHTML = '<li>Nothing was found :(</li>';
-		    } 
-		    Array.prototype.forEach.call(markers, function(markerElem) {
-		      	var id = markerElem.getAttribute('id');
-		      	var name = markerElem.getAttribute('name');
-		      	var address = markerElem.getAttribute('address');
-		      	var type = markerElem.getAttribute('type');
-		      	var point = new google.maps.LatLng(
-		        	parseFloat(markerElem.getAttribute('lat')),
-		        	parseFloat(markerElem.getAttribute('lng'))
-		      	);
-		      	var infoWinContent = document.createElement('div');
-		      	var strong = document.createElement('strong');
-		      	strong.textContent = name
-		      	infoWinContent.appendChild(strong);
-		      	infoWinContent.appendChild(document.createElement('br'));
-		      	var text = document.createElement('text');
-		      	text.textContent = address
-		      	infoWinContent.appendChild(text);
-		      	var icon = customLabel[type] || {};
-		      	var marker = new google.maps.Marker({
-		        	map: map,
-		        	position: point,
-		        	label: icon.label
-		      	});
-		      	marker.addListener('click', function() {
-		        	infoWindow.setContent(infoWinContent);
-		        	infoWindow.open(map, marker);
-		      	});
-		      	places.innerHTML += '<li>'+name+', '+address+', '+type+'</li>';      
-		    });
-		});
+            downloadLocations('get.php', function(data) {
+                places.innerHTML = '';			
+                var xml = data.responseXML;     
+                var markers = xml.documentElement.getElementsByTagName('marker');
+                console.log(markers);
+                if(markers.length === 0) {
+                    places.innerHTML = '<li>Nothing was found :(</li>';
+                } 
+                Array.prototype.forEach.call(markers, function(markerElem) {
+                    var id = markerElem.getAttribute('id');
+                    var name = markerElem.getAttribute('name');
+                    var address = markerElem.getAttribute('address');
+                    var type = markerElem.getAttribute('type');
+                    var point = new google.maps.LatLng(
+                        parseFloat(markerElem.getAttribute('lat')),
+                        parseFloat(markerElem.getAttribute('lng'))
+                    );
+                    var infoWinContent = document.createElement('div');
+                    var strong = document.createElement('strong');
+                    strong.textContent = name
+                    infoWinContent.appendChild(strong);
+                    infoWinContent.appendChild(document.createElement('br'));
+                    var text = document.createElement('text');
+                    text.textContent = address
+                    infoWinContent.appendChild(text);
+                    var icon = customLabel[type] || {};
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: point,
+                        label: icon.label
+                    });
+                    marker.addListener('click', function() {
+                        infoWindow.setContent(infoWinContent);
+                        infoWindow.open(map, marker);
+                    });
+                    places.innerHTML += '<li>'+name+', '+address+', '+type+'</li>';      
+                });
+            });
 
-		function downloadLocations(url, callback) {
-		  	var request = window.ActiveXObject ?
-		    	new ActiveXObject('Microsoft.XMLHTTP') :
-		    	new XMLHttpRequest;
-			request.open("GET", dataUrl, true);
-			request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			request.onreadystatechange = function() {
-		  		if(request.readyState == 4 && request.status == 200) {
-		      	request.onreadystatechange = doNothing;
-		      	callback(request, request.status);
-		  		}
-			}
-			request.send();
-		}
+            function downloadLocations(url, callback) {
+                var request = window.ActiveXObject ?
+                new ActiveXObject('Microsoft.XMLHTTP') :
+                new XMLHttpRequest;
+                request.open("GET", dataUrl, true);
+                request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                request.onreadystatechange = function() {
+                    if(request.readyState == 4 && request.status == 200) {
+                    request.onreadystatechange = doNothing;
+                    callback(request, request.status);
+                    }
+                }
+                request.send();
+            }
 	}
 
 	/*
 	Load current position
 	*/
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(load, error, userPosOptions);
+            navigator.geolocation.getCurrentPosition(load, error, userPosOptions);
 	} else {
-		alert("Geolocation is not supported by this browser.");
+            alert("Geolocation is not supported by this browser.");
 	}
 
 };
@@ -121,11 +121,11 @@ function init() {
 Update the value of distance field
 */
 function updateDistInput(val) {
-	document.getElementById('distInput').value = val;
+    document.getElementById('distInput').value = val;
 }
 
 function error(err) {
-	console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.warn(`ERROR(${err.code}): ${err.message}`);
 };
 
 /*
