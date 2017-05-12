01b87d846311eca15e5a13ef5c468280975d8f4e
Tomasz Mackiewicz <Tomasz.Mackiewicz@altkom.pl>
2017-05-12 16:52:11 +0200

    Add view scripts

---
+++ js/addScripts.js
@@ -0,0 +1,36 @@
+// function(){
+
+// 	var latlng = new google.maps.LatLng(52.2296756, 21.012228700000037);
+//   	var mapOptions = {
+//     	zoom: 15,
+//     	center: latlng
+//   	}
+//   	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//   	var geocoder = new google.maps.Geocoder();
+
+// 	function initialize() {
+// 	}
+
+// };
+
+// function addressToLocation() {
+
+//   var geocoder = new google.maps.Geocoder;
+//   var address = document.getElementById('address').value;
+
+//   geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == 'OK') {
+//       map.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//           map: map,
+//           position: results[0].geometry.location            
+//       });
+//       var lat = document.getElementById('lat');
+//       var long = document.getElementById('long');
+//       lat.value = results[0].geometry.location.lat();  
+//       long.value = results[0].geometry.location.lng();
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
